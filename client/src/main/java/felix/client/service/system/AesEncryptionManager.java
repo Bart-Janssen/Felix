590f0e05da83b5eb489feeea2acf080c42028145
Bart <bjanssen04@gmail.com>
2020-10-02 14:55:33 +0200

    Changed AES from CBC to GCM

--- client/src/main/java/felix/client/service/system/AesEncryptionManager.java
+++ client/src/main/java/felix/client/service/system/AesEncryptionManager.java
@@ -1,18 +1,27 @@
 package felix.client.service.system;
 
 import javax.crypto.Cipher;
-import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.GCMParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 import java.security.GeneralSecurityException;
 import java.security.SecureRandom;
+import java.security.spec.AlgorithmParameterSpec;
 import java.util.Base64;
 
 public class AesEncryptionManager
 {
+
+    private String key;
+    private static final String AES = "AES";
+    private static final int IV_SIZE = 12;
+    private static final int GCM_AUTHENTICATION_TAG = 128;
+    private static final String CIPHER_INSTANCE = "AES/GCM/NoPadding";
+
+    /*
     private String key;
     private static final int IV_SIZE = 128 / 8;
-    private static final String CIPHER_INSTANCE = "AES/CBC/PKCS5Padding";
-    private static final String AES = "AES";
+    private static final String CIPHER_INSTANCE = "AES/GCM/PKCS5Padding";
+    private static final String AES = "AES";*/
 
     public AesEncryptionManager(String key)
     {
@@ -20,6 +29,32 @@ public class AesEncryptionManager
     }
 
     public String encrypt(String plainText) throws GeneralSecurityException
+    {
+        byte[] iv = new byte[IV_SIZE];
+        new SecureRandom().nextBytes(iv);
+        Cipher cipher = Cipher.getInstance(CIPHER_INSTANCE);
+        GCMParameterSpec parameterSpec = new GCMParameterSpec(GCM_AUTHENTICATION_TAG, iv);
+        cipher.init(Cipher.ENCRYPT_MODE, new SecretKeySpec(Base64.getDecoder().decode(key), AES), parameterSpec);
+        byte[] encryptedText = cipher.doFinal(plainText.getBytes());
+        byte[] combinedEncryptedTextWithIv = new byte[IV_SIZE + encryptedText.length];
+        System.arraycopy(iv, 0, combinedEncryptedTextWithIv, 0, IV_SIZE);
+        System.arraycopy(encryptedText, 0, combinedEncryptedTextWithIv, IV_SIZE, encryptedText.length);
+        return Base64.getEncoder().encodeToString(combinedEncryptedTextWithIv);
+    }
+
+    public String decrypt(String combinedEncryptedTextWithIv) throws GeneralSecurityException
+    {
+        AlgorithmParameterSpec gcmIv = new GCMParameterSpec(GCM_AUTHENTICATION_TAG, Base64.getDecoder().decode(combinedEncryptedTextWithIv), 0, IV_SIZE);
+        byte[] combinedEncryptedTextWithIvBytes = Base64.getDecoder().decode(combinedEncryptedTextWithIv);
+        int encryptedSize = combinedEncryptedTextWithIvBytes.length - IV_SIZE;
+        byte[] encryptedText = new byte[encryptedSize];
+        System.arraycopy(combinedEncryptedTextWithIvBytes, IV_SIZE, encryptedText, 0, encryptedSize);
+        Cipher cipher = Cipher.getInstance(CIPHER_INSTANCE);
+        cipher.init(Cipher.DECRYPT_MODE, new SecretKeySpec(Base64.getDecoder().decode(key), AES), gcmIv);
+        return new String(cipher.doFinal(encryptedText));
+    }
+
+    /*public String encrypt(String plainText) throws GeneralSecurityException
     {
         byte[] iv = new byte[IV_SIZE];
         new SecureRandom().nextBytes(iv);
@@ -45,5 +80,5 @@ public class AesEncryptionManager
         Cipher cipher = Cipher.getInstance(CIPHER_INSTANCE);
         cipher.init(Cipher.DECRYPT_MODE, new SecretKeySpec(Base64.getDecoder().decode(key), AES), ivParameterSpec);
         return new String(cipher.doFinal(encryptedText));
-    }
+    }*/
 }
--- server/src/main/java/felix/api/configuration/AesEncryptionManager.java
+++ server/src/main/java/felix/api/configuration/AesEncryptionManager.java
@@ -2,14 +2,51 @@ package felix.api.configuration;
 
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
-import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.GCMParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 import java.security.GeneralSecurityException;
 import java.security.SecureRandom;
+import java.security.spec.AlgorithmParameterSpec;
 import java.util.Base64;
 
 public class AesEncryptionManager
 {
+    private static final int KEY_SIZE = 256;
+    private static final String AES = "AES";
+    private static final int IV_SIZE = 12;
+    private static final int GCM_AUTHENTICATION_TAG = 128;
+    private static final String CIPHER_INSTANCE = "AES/GCM/NoPadding";
+
+    public static String encrypt(String key, String plainText) throws GeneralSecurityException
+    {
+        byte[] iv = new byte[IV_SIZE];
+        new SecureRandom().nextBytes(iv);
+        Cipher cipher = Cipher.getInstance(CIPHER_INSTANCE);
+        GCMParameterSpec parameterSpec = new GCMParameterSpec(GCM_AUTHENTICATION_TAG, iv);
+        cipher.init(Cipher.ENCRYPT_MODE, new SecretKeySpec(Base64.getDecoder().decode(key), AES), parameterSpec);
+        byte[] encryptedText = cipher.doFinal(plainText.getBytes());
+        byte[] combinedEncryptedTextWithIv = new byte[IV_SIZE + encryptedText.length];
+        System.arraycopy(iv, 0, combinedEncryptedTextWithIv, 0, IV_SIZE);
+        System.arraycopy(encryptedText, 0, combinedEncryptedTextWithIv, IV_SIZE, encryptedText.length);
+        return Base64.getEncoder().encodeToString(combinedEncryptedTextWithIv);
+    }
+
+    public static String decrypt(String key, String combinedEncryptedTextWithIv) throws GeneralSecurityException
+    {
+        AlgorithmParameterSpec gcmIv = new GCMParameterSpec(GCM_AUTHENTICATION_TAG,
+                Base64.getDecoder().decode(combinedEncryptedTextWithIv), 0, IV_SIZE);
+        byte[] combinedEncryptedTextWithIvBytes = Base64.getDecoder().decode(combinedEncryptedTextWithIv);
+        int encryptedSize = combinedEncryptedTextWithIvBytes.length - IV_SIZE;
+        byte[] encryptedText = new byte[encryptedSize];
+        System.arraycopy(combinedEncryptedTextWithIvBytes, IV_SIZE, encryptedText, 0, encryptedSize);
+        Cipher cipher = Cipher.getInstance(CIPHER_INSTANCE);
+        cipher.init(Cipher.DECRYPT_MODE, new SecretKeySpec(Base64.getDecoder().decode(key), AES), gcmIv);
+        return new String(cipher.doFinal(encryptedText));
+    }
+
+    //Arrays.fill(key,(byte) 0); //overwrite the content of key with zeros
+
+    /*
     private static final int KEY_SIZE = 256;
     private static final int IV_SIZE = 128 / 8;
     private static final String CIPHER_INSTANCE = "AES/CBC/PKCS5Padding";
@@ -41,7 +78,7 @@ public class AesEncryptionManager
         Cipher cipher = Cipher.getInstance(CIPHER_INSTANCE);
         cipher.init(Cipher.DECRYPT_MODE, new SecretKeySpec(Base64.getDecoder().decode(key), AES), ivParameterSpec);
         return new String(cipher.doFinal(encryptedText));
-    }
+    }*/
 
     static String generateKey() throws GeneralSecurityException
     {
